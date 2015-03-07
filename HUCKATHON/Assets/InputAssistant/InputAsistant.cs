e44dd520761c00669f6728df6a3ef29af4ef4d0c
MrYD <azelf.trickroom@gmail.com>
2015-03-08 05:56:42 +0900

    インプットアシストを止められるようにした

--- HUCKATHON/Assets/InputAssistant/InputAsistant.cs
+++ HUCKATHON/Assets/InputAssistant/InputAsistant.cs
@@ -9,34 +9,62 @@ public class InputAsistant : MonoBehaviour
 {
     public GameObject Sphere;
     public GameObject GhostSphere;
-    public List<GameObject> SphereCloneList;
-    public float desPoint { get; set; }
-
+    private List<GameObject> SphereCloneList;
+    private float desPoint { get; set; }
     internal global::ExtendinWave GhostSphereClone1 { get; set; }
-
     internal global::WavingRing GhostSphereClone2 { get; set; }
+    private bool inputFlag;
+    private int time;
+
     // Use this for initialization
     void Start()
-	{
-	   SphereCloneList=new List<GameObject>();
-	    desPoint = -8;
-	   GhostSphereClone1=new ExtendinWave((GameObject) Instantiate(GhostSphere, new Vector3(desPoint, 0, 10), new Quaternion()));
-       GhostSphereClone1.gameObject.transform.localScale = Vector3.zero;
-       GhostSphereClone2 = new WavingRing((GameObject)Instantiate(GhostSphere, new Vector3(desPoint, 0, 10), new Quaternion()));
-      
-       
-
-	    for (int i = 1; i <= 5; i++)
-	    {
-            SphereCloneList.Add((GameObject)Instantiate(Sphere, new Vector3(12*i+desPoint, 0, 10), new Quaternion()));
-	    } 
-	}
+    {
+        SphereCloneList = new List<GameObject>();
+        desPoint = -8;
+        GhostSphereClone1 = new ExtendinWave((GameObject)Instantiate(GhostSphere, new Vector3(desPoint, 0, 10), new Quaternion()));
+        GhostSphereClone1.gameObject.transform.localScale = Vector3.zero;
+        GhostSphereClone2 = new WavingRing((GameObject)Instantiate(GhostSphere, new Vector3(desPoint, 0, 10), new Quaternion()));
+        for (int i = 1; i <= 5; i++)
+        {
+            SphereCloneList.Add((GameObject)Instantiate(Sphere, new Vector3(12 * i + desPoint, 0, 10), new Quaternion()));
+        }
+        inputFlag = false;
+        time = 0;
+    }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        time++;
+        if (time == 50) 
+        {
+            startInput();
+        }
+        if (time == 500)
+        {
+            endInput();
+        }
+        Input(inputFlag);
+    }
 
+    public void startInput()
+    {
+        inputFlag = true;
+    }
+
+    public void endInput()
+    {
+        inputFlag = false;
+    }
+
+
+    private void Input(bool inputFlag)
+    {
+       
         bool destroyFlag = false;
+        GhostSphereClone1.update(destroyFlag, 0.1f);
+        GhostSphereClone2.update(-0.002f);
+        if (!inputFlag) return; 
         foreach (var itr in SphereCloneList)
         {
             itr.transform.position -= new Vector3(0.1f, 0, 0);
@@ -55,8 +83,7 @@ public class InputAsistant : MonoBehaviour
                 destroyFlag = true;
             }
         }
-        GhostSphereClone1.update(destroyFlag, 0.1f);
-        GhostSphereClone2.update( -0.002f);
+       
 
         if (destroyFlag)
         {
@@ -68,6 +95,7 @@ public class InputAsistant : MonoBehaviour
     }
 
 
+   
 }
 
 internal class ExtendinWave
