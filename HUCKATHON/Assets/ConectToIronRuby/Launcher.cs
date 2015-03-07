ad9b3e3ae60bd11d331ae790489f5af6dac4f2bf
MrYD <azelf.trickroom@gmail.com>
2015-03-08 05:14:19 +0900

    no message

---
+++ HUCKATHON/Assets/ConectToIronRuby/Launcher.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConectToIronRuby
+{
+    static class Launcher
+    {
+        public static void  LaunchIronRuby()
+        {
+            System.Diagnostics.Process p = new System.Diagnostics.Process();
+
+            //入力できるようにする
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardInput = true;
+
+            //非同期で出力を読み取れるようにする
+            p.StartInfo.RedirectStandardOutput = true;
+            //p.OutputDataReceived += p_OutputDataReceived;
+
+            p.StartInfo.FileName =
+                System.Environment.CurrentDirectory + @"\Assets\ConectToIronRuby\ir64.exe";
+            p.StartInfo.CreateNoWindow = true;
+
+            //起動
+            p.Start();
+
+            //非同期で出力の読み取りを開始
+            p.BeginOutputReadLine();
+
+            //入力のストリームを取得
+            System.IO.StreamWriter sw = p.StandardInput;
+            if (sw.BaseStream.CanWrite)
+            {
+                //終了する
+                sw.WriteLine("exit");
+            }
+            sw.Close();
+
+            p.WaitForExit();
+        }
+    }
+}
---
+++ HUCKATHON/Assets/TextBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextBehaviour : MonoBehaviour {
+
+    public string InputText { get; set; } 
+
+	// Use this for initialization
+	void Start () {
+        RunOnRuby();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void RunOnRuby() {
+        ConectToIronRuby.Launcher.LaunchIronRuby();
+    }
+}
