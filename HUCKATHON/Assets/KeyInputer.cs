41b604dc027f454a5fc62be660dd35eab6d3cb4a
arlequin64 <arlequin.prx@gmail.com>
2015-03-08 14:57:18 +0900

    びちょうせい

---
+++ HUCKATHON/Assets/KeyInputer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyInputer : MonoBehaviour
+{
+	public string s;
+	public TextBehaviour tb;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.name == "unitychan_dynamic_locomotion")
+		{
+			tb.TypeKeys(this.name);
+		}
+	}
+}
--- HUCKATHON/Assets/TextBehaviour.cs
+++ HUCKATHON/Assets/TextBehaviour.cs
@@ -1,18 +1,91 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
 
-public class TextBehaviour : MonoBehaviour {
+public class TextBehaviour : MonoBehaviour
+{
+	public TextMesh oText;
+	public Boolean shiftflag;
 
     public string InputText { get; set; } 
 
 	// Use this for initialization
 	void Start () {
         RunOnRuby();
+		shiftflag = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	public void TypeKeys(string s)
+	{
+		switch (s)
+		{
+			case "space":
+				oText.text +=" ";
+				return;
+			case "enter":
+				oText.text += "\r\n";
+				return;
+			case "back":
+				oText.text = oText.text.Substring(0, oText.text.Length - 1);
+				return;
+			case "shift":
+				shiftflag = true;
+				return;
+			default :
+				if (shiftflag)
+				{
+					switch (s)
+					{
+						case "1":
+							oText.text += "!";
+							return;
+						case "2":
+							oText.text += "\"";
+							return;
+						case"3":
+							oText.text += "#";
+							return;
+						case"4":
+							oText.text += "$";
+							return;
+						case"5":
+							oText.text += "%";
+							return;
+						case"6":
+							oText.text += "&";
+							return;
+						case"7":
+							oText.text += "'";
+							return;
+						case"8":
+							oText.text += "(";
+							return;
+						case"9":
+							oText.text += ")";
+							return;
+						case",":
+							oText.text += "<";
+							return;
+						case".":
+							oText.text += ">";
+							return;
+						default:
+							oText.text += s.ToUpper();
+							return;
+					}
+					//oText.text += s.ToUpper();
+					shiftflag = false;
+				}
+				else
+					oText.text += s;
+				return;
+		}
+		
 	}
 
     void RunOnRuby() {
